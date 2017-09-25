6ca073b9dfc058b65a2cbed8f4187f4b3b77464e
Richard Marston <mail@richardmarston.net>
2017-09-25 07:30:24 +0000

    First commit

---
+++ sims4install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+grep wine /etc/apt/sources.list.d/additional-repositories.list > /dev/null
+if [ "$?" -ne "0" ];
+then
+  wget https://dl.winehq.org/wine-builds/Release.key
+  sudo apt-key add Release.key
+  sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
+  sudo apt-get update
+  sudo apt-get install -y winehq-stable # --install-suggests
+else
+  echo wine installed
+fi
+#sudo apt-get install wine libntlm0 libgnutls-openssl27 libgnutls-dev
+
+ls OriginSetup.exe
+if [ "$?" -ne "0" ];
+then
+  wget https://download.dm.origin.com/origin/live/OriginSetup.exe
+else
+  echo origin already downloaded
+fi
+
+export WINEPREFIX=${HOME}/.sims4/
+export WINEARCH=win32
+winecfg
+
+ls winetricks
+if [ "$?" -ne "0" ];
+then
+  wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+  chmod +x winetricks
+else
+  echo winetricks already downloaded
+fi
+
+
